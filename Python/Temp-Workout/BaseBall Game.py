9c23df5adf64570a19ca5e6944f2e83bd41e7e66
sentiaix <knnxcs@gmail.com>
2025-05-13 22:16:17 +0900

    Feat: 아직 마무리 안됨됨

--- Python/Temp-Workout/BaseBall Game.py
+++ Python/Temp-Workout/BaseBall Game.py
@@ -1,11 +1,29 @@
 # Baseball Game enhanced Version
 # 넣고싶은거: 1. (O) 4개 숫자(기회 20번) 2. 타이머 (원하는대로 작동하지 않음)
+# 3. 소리 출력기능 추가하기 4. 하도 못하면 한글자씩 알려주기(앞부터)
 
 import random # random number를 pulling 하기 위해 사용
-secretLen = 4 # 무작위 숫자의 길이는 3 >> 4임
+import winsound as sd # beepsound를 출력하기 위함
+
+def high_beep(): # 높은 beep
+    fr = 10000    # range : 37 ~ 32767
+    du = 1000     # 1000 ms ==1second
+    sd.Beep(fr, du) # winsound.Beep(frequency, duration)
+
+def low_beep(): # 낮은 beep
+    fr_l = 2000
+    du_l = 1000
+    sd.Beep(fr_l, du_l)
+
+def low_long_beep(): # 낮고 느린 beep
+    fr_ll = 2000
+    du_ll = 3000
+    sd.Beep(fr_ll, du_ll)
+
+secretLen = 5 # 무작위 숫자의 길이는 3 >> 5임
 
 secretList = random.sample(range(10), secretLen)
-        # 0-9 중 3 >> 4개 랜덤 선택
+        # 0-9 중 3 >> 5개 랜덤 선택
 secret = '' # secret이라는 빈 문자열 변수 생성
 for i in range(secretLen):
     secret += str(secretList[i])
@@ -14,11 +32,22 @@ for i in range(secretLen):
 # print(secret) #테스트용
 
 #for {var} in range(start num, end num + 1, increase num):
-for chance in range(20, 0, -1): # 10 >> 20번의 기회
+for chance in range(50, 0, -1): # 10 >> 50번의 기회
 
     ## 추론 숫자가 올바르게 입력됐는지 검사
     while True:
-        guess = input(f"You have {chance} chance(s). Guess my three-digit number: ")
+        guess = input(f"You have {chance} chance(s). Guess my three-digit number: ", end='')
+        if chance <= 40: # 기회가 40부터 10씩 줄어들때마다 한글자씩 추가로 공개
+            print("Hint!: ")
+            print(f"{secretList[0]}", end='')
+            if chance <= 30:
+                print(f"{secretList[1]}", end='')
+                if chance <= 20:
+                    print(f"{secretList[2]}", end='')
+                    if chance <= 10:
+                        print(f"{secretList[3]}", end='')
+        print("\n")
+
         if len(guess) ==  secretLen and guess.isdigit():
             break
 
@@ -43,12 +72,17 @@ for chance in range(20, 0, -1): # 10 >> 20번의 기회
     if strike > 0:
         if ball > 0:
             print(f"{strike} strike(s) and {ball} ball(s)\n")
+            low_beep()
         else: # ball이 0이면
             print(f"{strike} strike(s)\n")
+            high_beep()
     else: # strike가 0이면
         if ball > 0:
             print(f"{ball} ball(s)\n")
+            low_beep()
         else: # strike ball 모두 0이면
             print("Out\n")
+            low_long_beep()
 else: ### For문에 대한 else.
     print('You failed to guess my number..')
+    print(f"{secret}")
